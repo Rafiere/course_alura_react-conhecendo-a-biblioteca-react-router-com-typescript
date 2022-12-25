28b5a181cd93d90ceb4593fb5be8ac53302f97d5
Rafiere <rafapgbr@gmail.com>
2022-12-24 21:57:25 -0300

    refactor! refactoring code for ESLint format.

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,12 @@
+import cardapio from "pages/Cardapio/Itens/itens.json";
+
+const Inicio = () => {
+  return (
+    <section>
+      <h3>Recomendações da Cozinha</h3>
+      <div></div>
+    </section>
+  );
+};
+
+export default Inicio;
