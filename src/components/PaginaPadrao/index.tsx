4efdf8517f3d01e67ca275f33cea3cd28ff31ccc
Rafiere <rafapgbr@gmail.com>
2022-12-25 20:31:48 -0300

    feat: adding last lesson files

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -3,13 +3,15 @@ import styles from "./PaginaPadrao.module.scss";
 
 import { Outlet } from "react-router-dom";
 
+import stylesTema from "styles/Tema.module.scss";
+
 const PaginaPadrao = () => {
   return (
     <>
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
-      <div>
+      <div className={stylesTema.container}>
         {/* O "Outlet" contém todos os elementos "children" das rotas em que o "PaginaPadrao" é um elemento-pai. */}
         <Outlet />
       </div>
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,6 +1,11 @@
 import cardapio from "data/cardapio.json";
 
 import styles from "./Inicio.module.scss";
+import stylesTema from "styles/Tema.module.scss";
+
+import nossaCasa from "assets/casa.png";
+
+import { useNavigate } from "react-router-dom";
 
 const Inicio = () => {
   /* Estamos utilizando o "[...cardapio]" para, ao escolhermos três itens aleatórios do cardápio, não modificarmos o array original, e sim, criarmos um novo array apenas com os itens escolhidos, garantindo a imutabilidade. */
@@ -11,9 +16,19 @@ const Inicio = () => {
     .sort(() => 0.5 - Math.random())
     .splice(0, 3);
 
+  const navigate = useNavigate();
+
+  const redirecionarParaDetalhes = (prato: typeof cardapio[0]) => {
+    navigate(`/prato/${prato.id}`, {
+      state: {
+        prato,
+      },
+    });
+  };
+
   return (
     <section>
-      <h3 className={styles.titulo}>Recomendações da Cozinha</h3>
+      <h3 className={stylesTema.titulo}>Recomendações da Cozinha</h3>
       <div className={styles.recomendados}>
         {pratosRecomendados.map((item) => {
           return (
@@ -21,10 +36,22 @@ const Inicio = () => {
               <div className={styles.recomendado__imagem}>
                 <img src={item.photo} alt={item.title} />
               </div>
-              <button className={styles.recomendado__botao}>Ver Mais</button>
+              <button
+                className={styles.recomendado__botao}
+                onClick={() => redirecionarParaDetalhes(item)}
+              >
+                Ver Mais
+              </button>
             </div>
           );
         })}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa Casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do Aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3185 <br /> <br /> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
